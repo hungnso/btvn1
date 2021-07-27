d6169d2f480e151cbbb10d0837a1b5960795b21a
Your Name <duonghung.tn1509@gmail.com>
2021-07-27 22:08:43 +0700

    ok

--- interger/main.js
+++ interger/main.js
@@ -43,29 +43,3 @@ const interger = new Interger('output','.first','.second');
 document.querySelector("button").addEventListener("click", () => {
   interger.find();
 });
-
-
-// const a = Number(prompt('Nhap a: '));
-// const b = Number(prompt('Nhap b: '));
-// let body = document.getElementsByTagName('body')[0];
-// body.innerHTML += `<div>Cac so nguyen to trong khoang ${a} va ${b} la: </div>`
-
-// for (let i = a; i <= b; i++) {
-//     let temp = 0;
-//     if (i == 2) {
-//         body.innerHTML += `<div>${i}</div>`
-//     }
-
-//     else if (i > 2 && i % 2 != 0) {
-//         for (let j = 3; j <= Math.sqrt(i); j += 2) {
-//             if (i % j == 0) {
-//                 temp = 1;
-//                 break;
-//             }
-//         }
-
-//         if (i > 1 && temp == 0) {
-//             body.innerHTML += `<div>${i}</div>`
-//         }
-//     }
-// }
